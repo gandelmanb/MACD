7bb3767a125a8f691f507702fec1f81230e81254
gandelmanb <48888384+gandelmanb@users.noreply.github.com>
2023-05-04 14:07:45 +0300

    Update MACD_DEBUG.py

--- MACD_DEBUG.py
+++ MACD_DEBUG.py
@@ -13,7 +13,7 @@ import plotly.graph_objs as go
   
 def main():
     try:
-        print("^^^BARUCH DEBUG^^^<br><br><br><br>")
+        print("^^^BARUCH DEBUG 01^^^<br><br><br><br>")
         # Load stock data
         STOCK_NAME = sys.argv[1]
         print("Stock Name: " + STOCK_NAME + "<br>")
